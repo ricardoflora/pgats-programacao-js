0ee22660b70b79490a70c51460f1be378dc6fdb0
Ricardo Flora <ricardoflora@gmail.com>
2025-06-20 13:57:53 -0300

    Add novos arquivos

---
+++ conceitos-js/operadores-adicionais.js
@@ -0,0 +1,12 @@
+/* 
+ >   - maior que
+  >=  - maior ou igual que
+  <   - menor que
+  <=  - menor ou igual que
+  ==  - igualdade somente de valores
+  !=  - diferença somente de valores
+  === - igualdade de valores e tipo de dado
+*/
+
+console.log(`10 > 5 = ${ 10 > 5 }`) // verdadeiro = true
+ console.log("10 > 5 = ${ 10 > 5 }") // verdadeiro = true
